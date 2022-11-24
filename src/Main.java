d561aec8fcb33d667aa88db08da10645ee95e64a
Andry <Wwee>
2022-11-24 15:55:50 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.net.InetAddress;
 import java.util.Date;
 import java.util.Scanner;
 public class Main {
-    public static String ipAddress; //   IPv4-адрес 192.168.223.30
-    public static int port = 6500;
+    // IPv4-адрес XXX.XXX.XXX.XXX
+    public static String ipAddress;
+    // PORT
+    public static int PORT = 6500;
     public static void main(String[] args) throws IOException {
         try {
             PGP pgp = new PGP();
-            Date datereg = new Date();
             Scanner scanip = new Scanner(System.in);
-            Scanner info = new Scanner(System.in);
             // Создание нового объекта user1 и передача введенных данных и проверка что они вводились
             System.out.println("Имя хоста: " + InetAddress.getLocalHost());
             System.out.print("Сервер  локальный (yes/no)? : " );
@@ -28,8 +28,8 @@ public class Main {
             User user1 = new User("userLogin", "nullo", 21,"+7(981)1323878", "qwerty");
             System.out.println("\n[Login: " + user1.getLogin() + "  Email:  " + user1.getEmail() + " Возраст: "
                     + user1.getAge() + "  Телефон: " + user1.getPhoneUser() + "  Password: " + user1.getPassword()
-                    + "]  [Time: " + datereg + "]");
-            up1.sendMessageUser(ipAddress, port, user1.getLogin());
+                    + "]  [Time: " + new Date().toString() + "]");
+            up1.sendMessageUser(ipAddress, PORT, user1.getLogin());
 
         }
         catch (RuntimeException rex){
