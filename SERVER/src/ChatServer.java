4c1badeabceb242d7244a2c59b8a9955c42855fc
Andry <67545441+Andru-hub@users.noreply.github.com>
2022-12-07 17:24:48 +0300

    Update ChatServer.java

--- SERVER/src/ChatServer.java
+++ SERVER/src/ChatServer.java
@@ -3,7 +3,9 @@ import java.io.*;
 import java.net.*;
 import java.security.*;
 import java.util.*;
-class ServerSomthing extends Thread {
+
+class ServerSomthing extends Thread implements Server{
+    MyLog myLog = new MyLog();
     PGP pgp = new PGP();
     // сокет, через который сервер общается с клиентом, кроме него - клиент и сервер никак не связаны
     private Socket socket;
@@ -12,7 +14,6 @@ class ServerSomthing extends Thread {
     // поток завписи в сокет
     private BufferedWriter out;
     private Cipher ServerDecryptCipher;
-
     private PublicKey publicKey;
 
     public ServerSomthing(Socket socket) throws IOException {
@@ -20,35 +21,47 @@ class ServerSomthing extends Thread {
         // если потоку ввода/вывода приведут к генерированию искдючения, оно проброситься дальше
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        // создам поток для сериализации публичного ключа сервера и очищает буфер (сбрасываем его содержимое в выходной
+        // поток (на клиент))
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(pgp.getPublicKey());
+        objectOutputStream.flush();
+
         start(); // вызываем run()
     }
     @Override
     public void run() {
-        String word, kay;
+        String word, sword;
         try {
             // чтение сериализованных данных из потока
             ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-            // считываем из потока объект
-            PublicKey pub = (PublicKey)objectInputStream.readObject();
-            //System.out.println("Kay: " + pub);
+            // считываем из потока объект (Получаем ключ от клиента)
+            PublicKey pubKayClient = (PublicKey)objectInputStream.readObject();
+            //System.out.println("pubKayClient: " + pubKayClient); // для отладки
+
             while (true) {
+                // получаем зашифрованное сообщение от клиента
                 word = in.readLine();
-                kay = pgp.decrypt(word, pub);
-                if(kay.equals("stop")) {
+                //System.out.println("polucheno ot klienta (encrypt): " + word); // для отладки
+                // расшифровываем сообщение от клиента (privateKey server'a)
+                word = pgp.decrypt(word);
+                // шифруем сообщение от клиента (PublicKey от клиента)
+                sword = pgp.encrypt(word, pubKayClient);
+                if(word.equals("stop")) {
                     this.downService(); // харакири
                     break; // если пришла пустая строка - выходим из цикла прослушки
                 }
-                // зашифрованное сообщение
-                System.out.println("ENCRYPT: " + word);
-                // расшифрованное сообщение
-                System.out.println("DECRYPT: " + kay);
+                System.out.println("polucheno ot klienta (decrypt): " + word);
+                //System.out.println("otpravleno klientu (encrypt): " + sword); // для отладки
                 for (ServerSomthing vr : ChatServer.serverList) {
-                    vr.send(kay); // отослать принятое сообщение с привязанного клиента всем остальным влючая его
+                    myLog.printStory(word);
+                    vr.send(sword); // отослать принятое сообщение с привязанного клиента всем остальным влючая его
                 }
             }
-        } catch (NullPointerException ignored) {
+        } catch (NullPointerException ignored) { }
 
-        } catch (Exception e) {
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -73,18 +86,14 @@ class ServerSomthing extends Thread {
         } catch (IOException ignored) {}
     }
 }
-class Story {
 
-}
 public class ChatServer {
-    public static final int PORT = 6500; // порт который мы прослушаваем
+    public static final int PORT = 9999; // порт который мы прослушаваем
     public static LinkedList<ServerSomthing> serverList = new LinkedList<>();
-    public static Story story; // история переписки
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(PORT);
-        story = new Story();
-        System.out.println("Server Started");
+        System.out.println("========================       Server Started      ========================");
         System.out.println("======================== Server information ===============================" +
                            "\nserver address " + InetAddress.getLocalHost() +
                            "\nport " + server.getLocalPort() +
